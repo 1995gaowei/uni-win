accca2af6439f4ba64bd465b2973820e373a7f9a
woshikemizhende <15850535810@163.com>
2017-03-22 17:10:38 +0800

    add nav component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav1></nav1>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import nav1 from './components/Nav'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    nav1
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,48 @@
+<template>
+        <el-row>
+            <el-col :span="6"><h2 class="logo">UniWin</h2></el-col>
+            <el-col :span="4" :offset="14">
+                <div>
+                    <el-menu theme="dark" :default-active="activeIndex" class="nav" mode="horizontal" @select="handleSelect">
+                        <el-submenu index="1">
+                            <template slot="title">员工1</template>
+                            <el-menu-item index="1-1"><i class="el-icon-setting"></i> 设置</el-menu-item>
+                            <el-menu-item index="1-2"><i class="el-icon-time"></i> 注销</el-menu-item>
+                        </el-submenu>
+                    </el-menu>
+                </div>
+            </el-col>
+        </el-row>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.el-row {
+    background-color:  #324157
+}
+
+.nav {
+    text-align: right
+}
+
+.logo {
+    color: #bfcbd9;
+    margin: 0;
+    line-height: 60px;
+    text-align: center;
+}
+</style>
