559a3f243d57998c40a1736f98528dfa755f8922
woshikemizhende <15850535810@163.com>
2017-04-02 23:08:50 +0800

    add material apply ui

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -4,7 +4,7 @@
             <template slot="title"><i class="el-icon-menu"></i>物料管理</template>
             <el-menu-item-group>
                 <el-menu-item index="/Material/MaterialList"><i class="el-icon-d-arrow-right"></i>物料管理</el-menu-item>
-                <el-menu-item index="/m/1-4"><i class="el-icon-d-arrow-right"></i>物料预定查询</el-menu-item>
+                <el-menu-item index="/Material/MaterialApplyList"><i class="el-icon-d-arrow-right"></i>物料申请历史</el-menu-item>
                 <el-menu-item index="1-5"><i class="el-icon-d-arrow-right"></i>供应商管理</el-menu-item>
                 <el-menu-item index="1-6"><i class="el-icon-d-arrow-right"></i>款式新增</el-menu-item>
                 <el-menu-item index="1-7"><i class="el-icon-d-arrow-right"></i>款式与物料</el-menu-item>
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
-import AddMaterial from '@/views/Material/AddMaterial'
 import MaterialList from '@/views/Material/MaterialList'
+import MaterialApplyList from '@/views/Material/MaterialApplyList'
 
 import MaterialInput from '@/views/MaterialIn/MaterialInput'
 import MaterialInputManagment from '@/views/MaterialIn/MaterialInputManagment'
@@ -23,6 +23,11 @@ export default new Router({
       path: '/Material/MaterialList',
       name: 'MaterialList',
       component: MaterialList
+    },
+    {
+      path: '/Material/MaterialApplyList',
+      name: 'MateraiApplyList', 
+      component: MaterialApplyList
     },
     {
       path: '/MaterialIn/MaterialInput',
