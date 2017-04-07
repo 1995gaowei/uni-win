fdca30e915d7baf69ea008d7e4ef9797e150db4a
woshikemizhende <15850535810@163.com>
2017-04-07 10:39:48 +0800

    material input&output history

---
+++ src/views/MaterialIn/MaterialInputList.vue
@@ -0,0 +1,60 @@
+<template>
+<div>
+    <el-input
+      placeholder="请输入查询信息（日期/物料/操作人）"
+      icon="search"
+      v-model="materialInputSearchInfo"
+      :style="{ 'margin-bottom': '20px', 'width': '30%', 'min-width': '150px' }"
+      @change="handleSearchMaterialInput">
+    </el-input>
+
+    <el-table :data="materialInputList" style="width:100%">
+        <el-table-column prop="materialInputDate" sortable label="入库日期" width="120" :formatter="dateFormatter"></el-table-column>
+        <el-table-column prop="material.materialCode" label="物料编码"></el-table-column>
+        <el-table-column prop="material.materialName" label="物料名称"></el-table-column>
+        <el-table-column prop="materialInputVol" label="入库数量" ></el-table-column>
+        <el-table-column prop="user.userName" label="操作人" ></el-table-column>
+        <el-table-column prop="warehouse.location" label="仓储位置" width="180"></el-table-column>
+        <el-table-column prop="materialInputComment" label="备注"></el-table-column>
+    </el-table>
+
+</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Api from '@/config/api'
+
+export default {
+  data () {
+    return{
+    materialInputSearchInfo: '',
+    materialInputList: [],
+    dialogMaterialVisible: false,
+    formLabelWidth: '120px',
+    };
+  },
+  created: function () {
+    this.fetchData();
+  },
+  methods:{
+    fetchData () {
+        Vue.http.get(Api.backend_url + '/Warehouse/materialInManage').then(response => {
+            this.materialInputList = response.body.data;
+            this._materialInputList = response.body.data;
+        }, response => {
+            console.log(response);
+        });
+    },
+    handleSearchMaterialInput () {
+        this.materialInputList = this._materialInputList.filter((el, idx, arr) => {
+            return el.material.materialName.indexOf(this.materialInputSearchInfo) >=0 || el.material.materialCode.indexOf(this.materialInputSearchInfo) >= 0 ||
+            el.materialInputDate.indexOf(this.materialInputSearchInfo) >= 0 || el.user.userName.indexOf(this.materialInputSearchInfo) >= 0
+        })
+    },
+    dateFormatter(row, column) {
+        return row.materialInputDate.substring(0, 10);
+    }
+  }
+}
+</script>
---
+++ src/views/MaterialOut/MaterialOutputList.vue
@@ -0,0 +1,60 @@
+<template>
+<div>
+    <el-input
+      placeholder="请输入查询信息（日期/物料/操作人）"
+      icon="search"
+      v-model="materialOutputSearchInfo"
+      :style="{ 'margin-bottom': '20px', 'width': '30%', 'min-width': '150px' }"
+      @change="handleSearchMaterialOutput">
+    </el-input>
+
+    <el-table :data="materialOutputList" style="width:100%">
+        <el-table-column prop="materialOutputDate" sortable label="出库日期" width="120" :formatter="dateFormatter"></el-table-column>
+        <el-table-column prop="material.materialCode" label="物料编码"></el-table-column>
+        <el-table-column prop="material.materialName" label="物料名称"></el-table-column>
+        <el-table-column prop="materialOutputVol" label="出库数量" ></el-table-column>
+        <el-table-column prop="user.userName" label="操作人" ></el-table-column>
+        <el-table-column prop="warehouse.location" label="仓储位置" width="180"></el-table-column>
+        <el-table-column prop="materialOutputComment" label="备注"></el-table-column>
+    </el-table>
+
+</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Api from '@/config/api'
+
+export default {
+  data () {
+    return{
+    materialOutputSearchInfo: '',
+    materialOutputList: [],
+    dialogMaterialVisible: false,
+    formLabelWidth: '120px',
+    };
+  },
+  created: function () {
+    this.fetchData();
+  },
+  methods:{
+    fetchData () {
+        Vue.http.get(Api.backend_url + '/Warehouse/materialOut_showMaterialOutputs').then(response => {
+            this.materialOutputList = response.body.data;
+            this._materialOutputList = response.body.data;
+        }, response => {
+            console.log(response);
+        });
+    },
+    handleSearchMaterialOutput () {
+        this.materialOutputList = this._materialOutputList.filter((el, idx, arr) => {
+            return el.material.materialName.indexOf(this.materialOutputSearchInfo) >=0 || el.material.materialCode.indexOf(this.materialOutputSearchInfo) >= 0 ||
+            el.materialOutputDate.indexOf(this.materialOutputSearchInfo) >= 0 || el.user.userName.indexOf(this.materialOutputSearchInfo) >= 0
+        })
+    },
+    dateFormatter(row, column) {
+        return row.materialOutputDate.substring(0, 10);
+    }
+  }
+}
+</script>
